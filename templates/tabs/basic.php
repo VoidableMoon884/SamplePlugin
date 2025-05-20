12b511495391eed5b11982569f84d977be20dd30
VoidableMoon884 <trash_02@nogacity.de>
2025-05-20 21:25:58 +0200

    added button

--- templates/tabs/basic.php
+++ templates/tabs/basic.php
@@ -2,13 +2,11 @@
   <h4 class="mt-3"><?php echo _("Basic settings"); ?></h4>
   <div class="row">
     <div class="mb-3 col-12 mt-2">
-      <div class="row">
-        <div class="col-12">
-          <?php echo htmlspecialchars($content); ?>
-        </div>
-      </div>
       <div class="test">
         Test
+      </div>
+      <div class="button">
+        <input class="btn btn-outline btn-primary" type="submit" name="saveSettings" value="Test Button">
       </div>
     </div>
   </div>
